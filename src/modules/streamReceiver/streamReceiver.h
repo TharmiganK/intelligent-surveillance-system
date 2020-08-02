405e3e4765fc7856135ae9c96d72db2e76b53dc6
lavinan26 <lavinan26@gmail.com>
2020-08-02 13:57:03 +0530

    mock stream receiver added

---
+++ src/modules/streamReceiver/streamReceiver.h
@@ -0,0 +1,20 @@
+#include "../frameManager/frameManager.h"
+#include "../packetManager/packetManager.h"
+#include "../decoder/decoder.h"
+#include <fstream>
+#include <thread>
+#include <X11/Xlib.h>
+#include <deque>
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavformat/avio.h>
+#include <libswscale/swscale.h>
+}
+
+class StreamReceiver {
+    public:
+        AVCodecContext *codec_ctx;
+        void setContext(AVCodecContext *codec_ctx);
+        void operator()(PacketManager *packetManager, FrameManager *frameManager);
+};
