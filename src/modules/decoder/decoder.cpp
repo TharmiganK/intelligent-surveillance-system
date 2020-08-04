8ceca53d0541926c928cd39ef0ae757175940ea2
TharmiganK <ktharmi176@gmail.com>
2020-08-04 15:57:50 +0530

    Initial decoder class

--- src/modules/decoder/decoder.cpp
+++ src/modules/decoder/decoder.cpp
@@ -1,13 +1,26 @@
-//This is a mock decoder
+/**
+    Intelligent Surveillance System
+    @file decoder.h
+    @author Tharmigan Krishnananthalingam
+*/
 
 #include "decoder.h"
 
-    Decoder::Decoder(AVCodecContext *codec_ctx) {
-            this->Decoder::codec_ctx = codec_ctx;
-    }
+// Decoder::Decoder(AVCodecContext *codec_context) {
+
+//     codec_ctx = codec_context;
+//     width = codec_ctx->width;
+//     height = codec_ctx->height;
+//     bufferImgSize = avpicture_get_size(AV_PIX_FMT_BGR24, codec_ctx->width, codec_ctx->height);
+//     buffer = (uint8_t*) av_mallocz(bufferImgSize);
+//     imgCvtCtx = sws_getContext(codec_ctx->width, Decoder::codec_ctx->height,
+//                     Decoder::codec_ctx->pix_fmt, Decoder::codec_ctx->width, Decoder::codec_ctx->height, AV_PIX_FMT_BGR24,
+//                     SWS_BICUBIC, NULL, NULL, NULL);
+ 
+// }
     
 
-    void Decoder::operator()(FrameManager *frameManager, PacketManager *packetManager) {
+void Decoder::operator()(FrameManager *frameManager, PacketManager *packetManager) {
                     int size = avpicture_get_size(AV_PIX_FMT_YUV420P, Decoder::codec_ctx->width,
                     Decoder::codec_ctx->height);
                     uint8_t* picture_buffer = (uint8_t*) (av_malloc(size));
@@ -48,4 +61,59 @@
                         av_free(picture_rgb);
                         av_free(picture_buffer);
                         av_free(picture_buffer_2);
+}
+
+/**
+    @details DecodeVideo method accepts the video packets from stream and decode
+    it to a YUV video frame.
+*/
+AVFrame* Decoder::DecodeVideo(const AVPacket* avpkt){
+
+    AVFrame *outFrameYUV = av_frame_alloc();
+
+    if (codec_ctx){
+
+        if (avpkt && outFrameYUV){
+
+            int got_picture_ptr = 0;
+            int videoFrameBytes = avcodec_decode_video2(codec_ctx, outFrameYUV, &got_picture_ptr, avpkt);
+
+        }
     }
+
+    return outFrameYUV;
+
+}
+
+/**
+    @details GetBGRFrame method accepts the YUV formatted decoded video frame and 
+    converts it to a BGR formatted video frame.
+*/
+AVFrame* Decoder::GetBGRFrame(AVFrame *frameYUV){
+
+    AVFrame * frame = NULL;
+    frame = av_frame_alloc();
+    
+    if (frame){
+
+        avpicture_fill((AVPicture*) frame, buffer, AV_PIX_FMT_BGR24, width, height);
+
+        sws_scale(imgCvtCtx, frameYUV->data, frameYUV->linesize, 0, height, frame->data, frame->linesize);
+        
+    }
+
+    return (AVFrame *) frame;
+
+}
+
+/**
+    @details GetImage method accepts the BGR formatted video frame and converts
+    it to a OpenCV Mat image.
+*/
+cv::Mat Decoder::GetImage(AVFrame *frameBGR){
+
+    cv::Mat image(codec_ctx->height, codec_ctx->width, CV_8UC3, frameBGR->data[0], frameBGR->linesize[0]);
+
+    return image;
+
+}
--- src/modules/decoder/decoder.h
+++ src/modules/decoder/decoder.h
@@ -1,20 +1,93 @@
-//This is a mock decoder
+/**
+    Intelligent Surveillance System
+    @file decoder.h
+    @author Tharmigan Krishnananthalingam
+*/
 
+#ifndef _DECODER_H
+#define _DECODER_H
 #include "../frameManager/frameManager.h"
 #include "../packetManager/packetManager.h"
 #include <fstream>
 #include <deque>
+
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavformat/avio.h>
 #include <libswscale/swscale.h>
 }
+
+using namespace std;
+
+/**
+    @class Class to decode video packets
+    @details Decoder contains a video codec to decode AVPackets to AVFrames.
+    Swsscale function is used to convert YUV to BGR. And OpenCV Mat function
+    is used to convert BGR frame to image Matrix.
+*/
 class Decoder {
 
+    private:
+
+        AVCodecContext *codec_ctx; /*!< Codec context information of the video codec */
+        struct SwsContext *imgCvtCtx; /*!< Image convert context information (YUV -> BGR) */
+        int width; /*!< Width of the decoded frame */
+        int height; /*!< Height of the decoded frame */
+        int bufferImgSize; /*!< Image buffer size from video codec */
+        uint8_t *buffer; /*!< Buffer image used for YUV to BGR conversion */
+
     public:
-        AVCodecContext *codec_ctx;
-        Decoder(AVCodecContext *codec_ctx);
+
+        /**
+            @brief Constructor of class Decoder.
+            @param codec_context Video codec context.
+            @details Constructor of Decoder defines the video codec context used for decoding, height and width for
+            the decoded frame, buffer image used to convert YUV to BGR and image convert context for YUV to BGR
+            conversion.
+        */
+        Decoder(AVCodecContext *codec_context){
+
+            codec_ctx = codec_context;
+
+            width = codec_ctx->width;
+            height = codec_ctx->height;
+
+            bufferImgSize = avpicture_get_size(AV_PIX_FMT_BGR24, codec_ctx->width, codec_ctx->height);
+            buffer = (uint8_t*) av_mallocz(bufferImgSize);
+
+            imgCvtCtx = sws_getContext(codec_ctx->width, Decoder::codec_ctx->height,Decoder::codec_ctx->pix_fmt, 
+                                        Decoder::codec_ctx->width, Decoder::codec_ctx->height, AV_PIX_FMT_BGR24,
+                                        SWS_BICUBIC, NULL, NULL, NULL);
+
+
+        }
+
+        /**
+            @brief Method to Decode video packet into YUV frame.
+            @return Decoded YUV frame.
+            @param avpkt Video Packet from the stream.
+        */
+        AVFrame *DecodeVideo(const AVPacket *avpkt);
+
+        /**
+            @brief Method to convert YUV frame to BGR frame.
+            @return Decoded BGR frame.
+            @param frameYUV Decoded YUV frame.
+        */
+        AVFrame *GetBGRFrame(AVFrame *frameYUV);
+
+        /**
+            @brief Method to convert BGR frame to Opencv Mat image.
+            @return OpenCV Mat image.
+            @param frameBGR Decoded BGR frame.
+        */
+        cv::Mat GetImage(AVFrame *frameBGR);
+
+    public:
+
         void operator()(FrameManager *frameManager, PacketManager *packetManager);
 
 };
+
+#endif
