aca129f7200f254ebcf015096b806f699dffe178
TharmiganK <ktharmi176@gmail.com>
2020-08-24 17:12:06 +0530

    Added comments.

--- src/modules/httpServer/httpServer.cpp
+++ src/modules/httpServer/httpServer.cpp
@@ -1,53 +1,71 @@
+/**
+    Intelligent Surveillance System
+    @file httpServer.cpp
+    @author Tharmigan Krishnananthalingam
+*/
+
 #include <atomic>
 #include <thread>
 #include <memory>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/log/trivial.hpp>
 
 #include "httpServer.hpp"
 
 using namespace boost;
 
-//create new work of io_service
-HttpServer::HttpServer()
-{
-  m_Work.reset(new asio::io_service::work(m_IOService));
+/**
+    @details constructor creates a new work for IO service.
+*/
+HttpServer::HttpServer(){
+
+    m_Work.reset(new asio::io_service::work(m_IOService));
+
 }
 
-//start the http server
-void HttpServer::Start(unsigned short port, unsigned int thread_pool_size)
-{
-  if(thread_pool_size <= 0)  return;
+/**
+    @details Start method initialize a HTTP server on the specified port and 
+    creates given number of threads for listening. 
+*/
+void HttpServer::Start(unsigned short port, unsigned int thread_pool_size){
 
-  //create and start HttpAcceptor for accepting connection requests
-  m_Acceptor.reset(new HttpAcceptor(m_IOService, port));
-  m_Acceptor->Start();
+    if(thread_pool_size <= 0)  return;
 
-  std::cout<<"Server started at address: 127.0.0.1, port: "<<port<<std::endl;
-  std::cout<<"Goto http://127.0.0.1:"<<port<<"/"<<std::endl;
+    //create and start HttpAcceptor for accepting connection requests
+    m_Acceptor.reset(new HttpAcceptor(m_IOService, port));
+    m_Acceptor->Start();
 
-  //create specified number of threads and add them to the pool
-  for(unsigned int i = 0; i < thread_pool_size; i++){
-      std::unique_ptr<std::thread> 
-             th(new std::thread([this]()
-               {
-                 //run the socket io service
-                 m_IOService.run();
-               })
-             );
+    BOOST_LOG_TRIVIAL(info) << "HTTP SERVER STARTED AT ADDRESS: 127.0.0.1, PORT :1234";
+    BOOST_LOG_TRIVIAL(info) << "GOTO http://127.0.0.1:1234/";
 
-          m_ThreadPool.push_back(std::move(th));
+    //create specified number of threads and add them to the pool
+    for(unsigned int i = 0; i < thread_pool_size; i++){
+        std::unique_ptr<std::thread> 
+            th(new std::thread([this]()
+                {
+                    //run the socket io service
+                    m_IOService.run();
+                })
+            );
+
+        m_ThreadPool.push_back(std::move(th));
     }
+
 }
 
-//stop the server.
-void HttpServer::Stop()
-{
-  // m_Acceptor->Stop();
-  // m_IOService.stop();
-  for(auto& th : m_ThreadPool){
-    th->join();
-  }
+/**
+    @details Stop method stops the IO Service and Rquest Acceptor and join all 
+    the running threads to finish.
+*/
+void HttpServer::Stop(){
+    // m_Acceptor->Stop();
+    // m_IOService.stop();
+
+    for(auto& th : m_ThreadPool){
+        th->join();
+    }
+
 }
 
 
