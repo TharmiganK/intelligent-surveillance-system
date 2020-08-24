87eb0dc30a088bd00664d1dde10157a947c2f315
TharmiganK <ktharmi176@gmail.com>
2020-08-24 17:38:45 +0530

    Added comments.

--- src/modules/httpServer/httpAcceptor.hpp
+++ src/modules/httpServer/httpAcceptor.hpp
@@ -1,27 +1,56 @@
-#ifndef HTTPACCEPTOR_HPP
-#define HTTPACCEPTOR_HPP
+/**
+    Intelligent Surveillance System
+    @file httpAcceptor.h
+    @author Tharmigan Krishnananthalingam
+*/
+
+#ifndef _HTTPACCEPTOR_HPP
+#define _HTTPACCEPTOR_HPP
 
 #include <string>
 #include <memory>
 #include <atomic>
 #include <boost/asio.hpp>
 
-//a HTTP coonection acceptor
-class HttpAcceptor
-{
-  public:
-    HttpAcceptor(boost::asio::io_service&, unsigned short);
-    void Start();
-    void Stop();
+/**
+    @class Class to accept a HTTP requests from sockets.
+    @details HttpAcceptor class can be used to accept HTTP requests and connect
+    to clients. This class is created using the Boost/ASIO library.
+*/
+class HttpAcceptor{
 
-  private:
-    void AcceptConnection();
+    private:
 
-  private:
-    boost::asio::io_service& m_IOService;
-    boost::asio::ip::tcp::acceptor m_Acceptor;
-    std::atomic<bool> m_IsStopped;
+        boost::asio::io_service& m_IOService; /*!< IOService to connect sockets */
+        boost::asio::ip::tcp::acceptor m_Acceptor; /*!< TCP/IP request acceptor */
+        std::atomic<bool> m_IsStopped; /*!< Boolean value to indicate whether acceptor is running or not */
+
+    public:
+
+        /**
+            @brief Constructor of the class HttpAcceptor.
+            @param ios initiated IO Service.
+            @param port_num port number of the HTTP server.
+        */
+        HttpAcceptor(boost::asio::io_service& ios, unsigned short port_num);
+
+        /**
+            @brief Starting the HttpAcceptor
+        */
+        void Start();
+
+        /**
+            @brief Stopping the HttpAcceptor
+        */
+        void Stop();
+
+    private:
+        
+        /**
+            @brief accept a async connection with a client
+        */
+        void AcceptConnection();
+  
 };
-
 
 #endif
