8b2b3b020fa7e8dc3d5fdaf3d55e0a58f41a5815
TharmiganK <ktharmi176@gmail.com>
2020-08-24 19:34:37 +0530

    Added comments.

--- src/modules/httpServer/httpService.h
+++ src/modules/httpServer/httpService.h
@@ -1,5 +1,12 @@
-#ifndef HTTPSERVICE_HPP
-#define HTTPSERVICE_HPP
+/**
+    Intelligent Surveillance System
+    @file httpService.h
+    @author Tharmigan Krishnananthalingam
+*/
+
+
+#ifndef _HTTPSERVICE_HPP
+#define _HTTPSERVICE_HPP
 
 #include <string>
 #include <memory>
@@ -7,71 +14,131 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
-// #include "../streamReceiver/streamReceiver.h"
-// #include "../display/display.h"
-// #include "../decoder/decoder.h"
-// #include "../frameQueue/frameQueue.h"
-// #include "../packetQueue/packetQueue.h"
 #include "../videoStream/videoStream.h"
-// #include "../outputStreamer/outputStreamer.h"
 #include "../processor/processor.h"
 
-//http methods types
+/**
+    http methods types
+*/
 enum class HttpMethods{
-  GET,
-  HEAD,
-  POST,
-  PUT,
-  DELETE,
-  CONNECT,
-  OPTIONS,
-  TRACE,
+
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+
 };
 
-//each http request returned by parser
-struct HttpRequest
-{
-  HttpMethods method;
-  std::string request;
-  std::string resource;
-  std::string http_version;
-  boost::property_tree::ptree body;
-  unsigned int status;
+/**
+    each http request returned by parser
+*/
+struct HttpRequest{
+
+    HttpMethods method; /*!< Http method */
+    std::string request;  /*!< Http Request string */
+    std::string resource;  /*!< Http Request resource path */
+    std::string http_version;  /*!< Http version */
+    boost::property_tree::ptree body;  /*!< Http Request body */
+    unsigned int status;  /*!< Http Request status */
+
 };
 
-class HttpRequestParser
-{
-  public:
-    HttpRequestParser(std::string&);
-    std::shared_ptr<HttpRequest> GetHttpRequest();
+/**
+    @class Class to parse a HTTP request (string -> HttpRequest object)
+    @details HttpRequestParser class can be used to parse a string HTTP request
+    into a HttpRequest object.
+*/
+class HttpRequestParser{
 
-  private:
-    std::string m_HttpRequest;
+    private:
+
+        std::string m_HttpRequest; /*!< Http Request string */
+
+    public:
+
+        /**
+            @brief Constructor of HttpRequestParser.
+            @param request HTTP request string.
+        */
+        HttpRequestParser(std::string& request);
+
+        /**
+            @brief Get the parsed HttpRequest object.
+            @return Pointer of HttpRequset object.
+        */
+        std::shared_ptr<HttpRequest> GetHttpRequest();
+
 };
 
-class HttpService
-{
-  public:
-    HttpService(std::shared_ptr<boost::asio::ip::tcp::socket>);
+/**
+    @class Class to handle HTTP requests and services.
+    @details HttpService class contains methods to handle different kind of HTTP 
+    requests. And defines the services of the server.
+*/
+class HttpService{
 
-    void HttpHandleRequest();
+    private:
+
+        std::shared_ptr<boost::asio::ip::tcp::socket> m_Socket; /*!< TCP/IP network socket */
+        boost::asio::streambuf m_Request; /*!< Http Request String */
+        std::string m_RequestedResource; /*!< Http Requested resource path */
+        unsigned int m_ResponseStatusCode; /*!< Http Response status */
+        bool m_IsResponseSent; /*!< Boolean value to determine whether response is sent or not */
+        std::string m_ServerOptions; /*!< Http server options */
+        std::string m_ContentType; /*!< Http content type */
+
+    public:
+
+        /**
+            @brief Constructor of HttpService.
+            @param sock TCP/IP network socket.
+        */
+        HttpService(std::shared_ptr<boost::asio::ip::tcp::socket> sock);
+
+        /**
+            @brief Handles the HTTP requests.
+        */
+        void HttpHandleRequest();
   
-  private:
-    std::string GetIp();
-    void ProcessPostRequest(std::shared_ptr<HttpRequest> httpRequest);
-    void addStream(std::shared_ptr<HttpRequest> httpRequest);
-    std::string GetResponseStatus();
-    void SendResponse();
-    void Finish();
+    private:
+        
+        /**
+            @brief Returns the client IP address.
+            @return IP address of the client in string.
+        */
+        std::string GetIp();
 
-  private:
-    std::shared_ptr<boost::asio::ip::tcp::socket> m_Socket;
-    boost::asio::streambuf m_Request;
-    std::string m_RequestedResource;
-    unsigned int m_ResponseStatusCode;
-    bool m_IsResponseSent;
-    std::string m_ServerOptions;
-    std::string m_ContentType;
+        /**
+            @brief Returns the Response status.
+            @return Response status in string.
+        */
+        std::string GetResponseStatus();
+
+        /**
+            @brief Sends the response to the client.
+        */
+        void SendResponse();
+
+        /**
+            @brief Deletes the current object.
+        */
+        void Finish();
+
+        /**
+            @brief Processes the POST requests from client.
+            @param httpRequest HttpRequest object which contains the request info.
+        */
+        void ProcessPostRequest(std::shared_ptr<HttpRequest> httpRequest);
+
+        /**
+            @brief Service to add streams in Processor.
+            @param httpRequest HttpRequest object which contains the request info.
+        */
+        void addStream(std::shared_ptr<HttpRequest> httpRequest);
 
 };
 
