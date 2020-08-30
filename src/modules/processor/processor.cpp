1048c557a7737b0ae7d786e6c30399d83b698942
TharmiganK <ktharmi176@gmail.com>
2020-08-30 20:51:24 +0530

    Commented the display function.

--- src/modules/processor/processor.cpp
+++ src/modules/processor/processor.cpp
@@ -15,7 +15,7 @@ void Processor::process() {
     XInitThreads();
     streamReceiver1 = new std::thread(StreamReceiver(),std::ref(videoStreams));
     decoder1 = new std::thread(Decoder(),std::ref(videoStreams));
-	display1 = new std::thread(display(), std::ref(videoStreams));
+	// display1 = new std::thread(display(), std::ref(videoStreams));
     outputStreamer1 = new std::thread(Streamer(), std::ref(videoStreams));
 
 }
